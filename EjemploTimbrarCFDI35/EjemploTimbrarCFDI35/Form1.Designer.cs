ab1cbfa939a76b323c6ed1e2716c2b5e5f2fd631
Luigimon <almadejaguar@hotmail.com>
2013-01-21 13:44:38 -0600

    Agregado version ejemplo 3.5

---
+++ EjemploTimbrarCFDI35/EjemploTimbrarCFDI35/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace EjemploTimbrarCFDI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTimbrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstErrores = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pbQR = new System.Windows.Forms.PictureBox();
+            this.cbQR = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUUID = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lblCancelar = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTimbrar
+            // 
+            this.btnTimbrar.Location = new System.Drawing.Point(12, 12);
+            this.btnTimbrar.Name = "btnTimbrar";
+            this.btnTimbrar.Size = new System.Drawing.Size(75, 23);
+            this.btnTimbrar.TabIndex = 0;
+            this.btnTimbrar.Text = "Timbrar";
+            this.btnTimbrar.UseVisualStyleBackColor = true;
+            this.btnTimbrar.Click += new System.EventHandler(this.btnTimbrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Errores:";
+            // 
+            // lstErrores
+            // 
+            this.lstErrores.FormattingEnabled = true;
+            this.lstErrores.Location = new System.Drawing.Point(12, 67);
+            this.lstErrores.Name = "lstErrores";
+            this.lstErrores.Size = new System.Drawing.Size(395, 121);
+            this.lstErrores.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Resultado";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(12, 207);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(395, 193);
+            this.txtResultado.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 403);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "QRCode";
+            // 
+            // pbQR
+            // 
+            this.pbQR.Location = new System.Drawing.Point(15, 429);
+            this.pbQR.Name = "pbQR";
+            this.pbQR.Size = new System.Drawing.Size(148, 148);
+            this.pbQR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbQR.TabIndex = 6;
+            this.pbQR.TabStop = false;
+            // 
+            // cbQR
+            // 
+            this.cbQR.AutoSize = true;
+            this.cbQR.Location = new System.Drawing.Point(67, 403);
+            this.cbQR.Name = "cbQR";
+            this.cbQR.Size = new System.Drawing.Size(95, 17);
+            this.cbQR.TabIndex = 7;
+            this.cbQR.Text = "Crear QRCode";
+            this.cbQR.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(182, 429);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "UUID";
+            // 
+            // txtUUID
+            // 
+            this.txtUUID.Location = new System.Drawing.Point(185, 446);
+            this.txtUUID.Name = "txtUUID";
+            this.txtUUID.ReadOnly = true;
+            this.txtUUID.Size = new System.Drawing.Size(222, 20);
+            this.txtUUID.TabIndex = 9;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(309, 473);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(97, 23);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "Cancelar Timbre";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // lblCancelar
+            // 
+            this.lblCancelar.AutoSize = true;
+            this.lblCancelar.Location = new System.Drawing.Point(182, 508);
+            this.lblCancelar.Name = "lblCancelar";
+            this.lblCancelar.Size = new System.Drawing.Size(0, 13);
+            this.lblCancelar.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 581);
+            this.Controls.Add(this.lblCancelar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.txtUUID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbQR);
+            this.Controls.Add(this.pbQR);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstErrores);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTimbrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbQR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTimbrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstErrores;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pbQR;
+        private System.Windows.Forms.CheckBox cbQR;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUUID;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label lblCancelar;
+    }
+}
+
---
+++ EjemploTimbrarCFDI35/EjemploTimbrarCFDI35/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FIGeneradorFacturaElectronica;
+using FIGeneradorFacturaElectronica.ComplementoConcepto;
+using FIGeneradorFacturaElectronica.Complementos;
+
+
+namespace EjemploTimbrarCFDI
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        //Datos Certificado
+        private String ArchivoKey = "aaa010101aaa__csd_01.key";
+        private String ArchivoCertificado = "aaa010101aaa__csd_01.cer";
+        private String ContraseñaCertificado = "12345678a";
+        
+        private String PosicionCertificado = "D:\\Certificados\\";
+        private string RFC = "aaa010101aaa";
+
+        //datos Timbre
+        private String CodigoUsuarioProveedor = "N#@Mo!)#oh>)BYOdX=q_ZUCsLxqpv?";
+        private String CodigoUsuario = "1763AAB0593430490B3B3EE5457A9A2580F9D7DE";
+        private int IdSucursal = 151048;
+
+        private void btnTimbrar_Click(object sender, EventArgs e)
+        {
+            
+            //crear un nuevo comprobante CFDI para la version 3.2
+            FIGeneradorFacturaElectronica.ComprobanteCFDI32 Comprobante=new ComprobanteCFDI32();
+            Comprobante.serie = "MER";
+            Comprobante.folio = "8223"; //dato no obligatorio
+            Comprobante.fecha = System.DateTime.Now;
+            Comprobante.formaDePago = "Pago en una sola exhibición";
+            Comprobante.subTotal = new FIGeneradorFacturaElectronica.Importe(1100);
+            Comprobante.total = new FIGeneradorFacturaElectronica.Importe(1276);
+            Comprobante.tipoDeComprobante = "ingreso";
+            Comprobante.Moneda = "MXN";
+            //Comprobante.TipoCambio = "1"; //utilizar en caso de moneda extranjera
+            
+
+            // Campos nuevos en comprobante
+            Comprobante.LugarExpedicion = "VERACRUZ";
+            Comprobante.NumCtaPago = "NO IDENTIFICADO";
+            Comprobante.metodoDePago = "NO IDENTIFICADO";
+
+            //Campo nuevo en Emisor
+            Comprobante.Emisor.RegimenFiscal.Add("REGIMEN GENERAL DE LEY PERSONAS MORALES");
+
+            //Llenado de datos del emisor
+            Comprobante.Emisor.rfc = RFC;
+            Comprobante.Emisor.nombre = "Empresa de prueba";
+            Comprobante.Emisor.DomicilioFiscal.calle = "Av. Paseo de la Reforma";
+            Comprobante.Emisor.DomicilioFiscal.noExterior = "89";
+            Comprobante.Emisor.DomicilioFiscal.noInterior = "103";
+            Comprobante.Emisor.DomicilioFiscal.colonia = "Cuauhtemoc";
+            Comprobante.Emisor.DomicilioFiscal.localidad = "Ciudad de Mexico";
+            Comprobante.Emisor.DomicilioFiscal.municipio = "Cuauhtemoc";
+            Comprobante.Emisor.DomicilioFiscal.estado = "Distrito Federal";
+            Comprobante.Emisor.DomicilioFiscal.pais = "MEXICO";
+            Comprobante.Emisor.DomicilioFiscal.codigoPostal = "06500";
+
+            Comprobante.Emisor.ExpedidoEn.calle = "FRANCISCO PEREZ";
+            Comprobante.Emisor.ExpedidoEn.noExterior = "35";
+            Comprobante.Emisor.ExpedidoEn.noInterior = "1";
+            Comprobante.Emisor.ExpedidoEn.colonia = "RICARDO FLORES MAGON";
+            Comprobante.Emisor.ExpedidoEn.localidad = "VERACRUZ, VER.";
+            Comprobante.Emisor.ExpedidoEn.municipio = "VERACRUZ";
+            Comprobante.Emisor.ExpedidoEn.estado = "Veracruz";
+            Comprobante.Emisor.ExpedidoEn.pais = "México";
+            Comprobante.Emisor.ExpedidoEn.codigoPostal = "91900";
+
+            //Llenado de datos del receptor
+            Comprobante.Receptor.rfc = "FID080111867";
+            Comprobante.Receptor.nombre = "FREIGHTIDEAS S.A DE C.V.";
+            Comprobante.Receptor.Domicilio.calle = "ARIZONA";
+            Comprobante.Receptor.Domicilio.noExterior = "108";
+            //Comprobante.Receptor.Domicilio.noInterior = "1";
+            Comprobante.Receptor.Domicilio.colonia = "COL. NAPOLES";
+            //Comprobante.Receptor.Domicilio.localidad = "TEOLOYUCAN";
+            Comprobante.Receptor.Domicilio.municipio = "Benito Juarez";
+            Comprobante.Receptor.Domicilio.estado = "Distrito Federal";
+            Comprobante.Receptor.Domicilio.pais = "MEXICO";
+            Comprobante.Receptor.Domicilio.codigoPostal = "03810";
+
+
+            //Crear un nuevo concepto
+            FIGeneradorFacturaElectronica.Concepto Concepto1 = new FIGeneradorFacturaElectronica.Concepto();
+            Concepto1.cantidad = 1;
+            Concepto1.descripcion = "Software";
+            Concepto1.valorUnitario = new FIGeneradorFacturaElectronica.Importe(1000);
+            Concepto1.importe = new FIGeneradorFacturaElectronica.Importe(1000);
+            //Concepto1.noIdentificacion = "PINO";
+            //nuevo en los conceptos
+            Concepto1.unidad = "Pieza";
+
+            //ejemplo para agregar una cuenta predial al concepto
+            //Concepto1.CuentaPredial="rur85859";
+
+            //ejemplo para agregarle informacion aduanera en el concepto
+            FIGeneradorFacturaElectronica.t_InformacionAduanera informacionAduanera=new t_InformacionAduanera();
+            informacionAduanera.numero = "120";
+            informacionAduanera.fecha = System.DateTime.Now;
+            informacionAduanera.numero = "234";
+            informacionAduanera.aduana = "AICM";
+
+            Parte parte=new Parte();
+            parte.InformacionAduanera.Add(informacionAduanera);
+            parte.cantidad = 1;
+            parte.descripcion = "Prueba";
+
+            Concepto1.Parte.Add(parte);
+            
+            //crear otro concepto
+             FIGeneradorFacturaElectronica.Concepto Concepto2 = new FIGeneradorFacturaElectronica.Concepto();
+             Concepto2.cantidad=1;
+             Concepto2.descripcion = "VALIDACION";
+             Concepto2.valorUnitario=new FIGeneradorFacturaElectronica.Importe(100);
+             Concepto2.importe = new FIGeneradorFacturaElectronica.Importe(100);
+             Concepto2.unidad = "No aplica";
+             
+            //agregando complemento concepto al comprobante por order y cuenta de terceros
+            AgregarComplementoterceros(Concepto2);
+
+            //crear otro concepto
+            FIGeneradorFacturaElectronica.Concepto Concepto3 = new FIGeneradorFacturaElectronica.Concepto();
+            Concepto3.cantidad = 1;
+            Concepto3.descripcion = "Factura";
+            Concepto3.valorUnitario = new FIGeneradorFacturaElectronica.Importe(1.50);
+            Concepto3.importe = new FIGeneradorFacturaElectronica.Importe(1.50);
+            Concepto3.unidad = "Pieza";
+
+            //Invocamos a la muestra de como crear complemento iedu
+            //AgregarComplementoiedu(Concepto3);
+
+            //Agregando los conceptos al comprobante
+            Comprobante.Conceptos.Add(Concepto1);
+            Comprobante.Conceptos.Add(Concepto2);
+            Comprobante.Conceptos.Add(Concepto3);
+            
+            //totales de impuestos
+            Comprobante.Impuestos.totalImpuestosRetenidos = new FIGeneradorFacturaElectronica.Importe(2317.44);
+            Comprobante.Impuestos.totalImpuestosTrasladados = new FIGeneradorFacturaElectronica.Importe(176);
+
+            //nuevo impuesto de tipo Traslado
+            FIGeneradorFacturaElectronica.Traslado Traslado = new FIGeneradorFacturaElectronica.Traslado();
+            Traslado.impuesto = FIGeneradorFacturaElectronica.Traslado.TipoImpuesto.IVA;
+            Traslado.tasa = new FIGeneradorFacturaElectronica.Importe(16);
+            Traslado.importe = new FIGeneradorFacturaElectronica.Importe(176);
+
+            //agregar el impuesto 
+            Comprobante.Impuestos.Traslados.Add(Traslado);
+
+
+            //Nuevo impuesto tipo retencion
+            FIGeneradorFacturaElectronica.Retencion Retencion = new FIGeneradorFacturaElectronica.Retencion();
+            Retencion.impuesto = FIGeneradorFacturaElectronica.Retencion.TipoImpuesto.IVA;
+            Retencion.importe = new FIGeneradorFacturaElectronica.Importe(2317.44);
+
+            //agregar retencion
+            Comprobante.Impuestos.Retenciones.Add(Retencion);
+
+            //invocamos a la muestra de como crear complemento de impuestos locales
+            //AgregarComplementoImpuestosLocales(Comprobante);
+
+            //Invocamos a la muestra de como crear complemento de donatarios
+            //AgregarComplementoDonatarios(Comprobante);
+
+            
+
+            //nuevo objeto para la generacion del CFDI especificando el tipo
+            FIGeneradorFacturaElectronica.Generador GenCFDI = new FIGeneradorFacturaElectronica.Generador(FIGeneradorFacturaElectronica.Generador.TipoFacturacion.CFDI);
+            
+            //Generar el nuevo comprobante y obtener el numero de errores
+            List<cErrores> Errores = GenCFDI.NuevoComprobante(Comprobante);
+
+            //si el listado de errores es mayor a 1 quiere decir que existen un error y no puede generarse el preCFDI
+            if(Errores.Count==0)
+            {
+                String PreCFDI=String.Empty;
+                cErrores ErroresPreCFDI =null;
+                //generar el preCFDI si es correcto se regresa true y puede ahora timbrase
+                if (GenCFDI.GenerarPreCFDI(PosicionCertificado + ArchivoKey, PosicionCertificado + ArchivoCertificado, ContraseñaCertificado,out PreCFDI,out ErroresPreCFDI))
+                {
+                    //Timbrando preCFDI
+                    String CFDITimbrado = String.Empty;
+                    //Conexion para timbrado
+                    FIGeneradorFacturaElectronica.Timbre Timbrado = new FIGeneradorFacturaElectronica.Timbre();
+                    //Si Timbrar devuelve true quiere decir que el timbre esta generado y podemos obtener las informacion del timbre de sus metodos
+                    if (Timbrado.Timbrar(CodigoUsuarioProveedor, CodigoUsuario, IdSucursal, PreCFDI, Comprobante.total.Valor.ToString(), out CFDITimbrado,cbQR.Checked))
+                    {
+                        String XMLTimbre = Timbrado.XMLTimbre; //nuestro timbre generado
+                        txtResultado.Text = CFDITimbrado; //nuestro CFDI ya timbrado
+                        txtUUID.Text = Timbrado.UUID;
+                        if (cbQR.Checked)
+                            pbQR.Image = Timbrado.QRImagen; //imagen QR
+                        lstErrores.Items.Clear();
+                        lstErrores.Items.Add("Sin errores");
+                    }
+                    else
+                    {
+                            lstErrores.Items.Clear();
+                            lstErrores.DataSource = Timbrado.Errores;
+                            lstErrores.ValueMember = "Error";
+                    }
+                }
+                else
+                {
+                    lstErrores.Items.Clear();
+                    lstErrores.Items.Add(ErroresPreCFDI.Descripcion);
+                }
+            }
+            else
+            {
+                lstErrores.Items.Clear();
+                lstErrores.DataSource = Errores;
+                lstErrores.DisplayMember = "Descripcion";
+            }
+
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            if(txtUUID.Text.Trim().Length>0)
+            {
+                //Conexion para timbrado
+                FIGeneradorFacturaElectronica.Timbre Timbrado = new FIGeneradorFacturaElectronica.Timbre();
+                //se envia a carcelar el timbre antes generado
+                String Resultado = String.Empty;
+                if (Timbrado.CancelarTimbre(CodigoUsuarioProveedor, CodigoUsuario, RFC, txtUUID.Text.Trim(), out Resultado))
+                    lblCancelar.Text = Resultado;
+                else
+                {
+                    
+                }
+            }
+        }
+
+        private void AgregarComplementoImpuestosLocales(ComprobanteCFDI32 Comprobante)
+        {
+            //Codigo de ejemplo para crear complemento de impuestos locales
+
+            FIGeneradorFacturaElectronica.Complementos.ImpuestosLocales ImpLoc=new FIGeneradorFacturaElectronica.Complementos.ImpuestosLocales();
+
+            //Crear valores de impuesto local
+            FIGeneradorFacturaElectronica.Complementos.TrasladosLocales TrasLoc = new FIGeneradorFacturaElectronica.Complementos.TrasladosLocales();
+            TrasLoc.ImpLocTrasladado = "ISH";
+            TrasLoc.TasadeTraslado = new FIGeneradorFacturaElectronica.Complementos.ImporteImpuestosLocales(10);
+            TrasLoc.Importe = new FIGeneradorFacturaElectronica.Complementos.ImporteImpuestosLocales(120.25);
+
+             
+            //Agregar el traslado
+            ImpLoc.TrasladosLocales.Add(TrasLoc); 
+
+            ImpLoc.TotaldeTraslados = new FIGeneradorFacturaElectronica.Complementos.ImporteImpuestosLocales(120.25);
+            ImpLoc.TotaldeRetenciones=new FIGeneradorFacturaElectronica.Complementos.ImporteImpuestosLocales(0);
+
+            cErrores Errores = null;
+            
+            if(!Comprobante.Complementos.AgregarComplemento(ImpLoc,out Errores))
+                lstErrores.DataSource = Errores;
+        }
+
+        private void AgregarComplementoterceros(Concepto cConcepto)
+        {
+            FIGeneradorFacturaElectronica.ComplementoConcepto.terceros Tercero=new terceros();
+            Tercero.rfc = "FID080111867";
+            Tercero.nombre = "FREIGHTIDEAS S.A DE C.V.";
+            Tercero.InformacionFiscalTercero.calle = "ARIZONA";
+            Tercero.InformacionFiscalTercero.noExterior = "108";
+            //Comprobante.Receptor.Domicilio.noInterior = "1";
+            Tercero.InformacionFiscalTercero.colonia = "COL. NAPOLES";
+            //Comprobante.Receptor.Domicilio.localidad = "TEOLOYUCAN";
+            Tercero.InformacionFiscalTercero.municipio = "Benito Juarez";
+            Tercero.InformacionFiscalTercero.estado = "Distrito Federal";
+            Tercero.InformacionFiscalTercero.pais = "MEXICO";
+            Tercero.InformacionFiscalTercero.codigoPostal = "03810";
+
+            Traslado TerceroTraslado=new Traslado();
+            TerceroTraslado.impuesto = Traslado.TipoImpuesto.IVA;
+            TerceroTraslado.tasa=new Importe(15);
+            TerceroTraslado.importe=new Importe(15);
+
+            Tercero.ImpuestosTerceros.Traslados.Add(TerceroTraslado);
+
+            cConcepto.ComplementoConcepto = Tercero;
+        }
+
+        private void AgregarComplementoDonatarios(ComprobanteCFDI32 Comprobante)
+        {
+            FIGeneradorFacturaElectronica.Complementos.donat Donatarios = new FIGeneradorFacturaElectronica.Complementos.donat();
+
+            Donatarios.fechaAutorizacion = System.DateTime.Now;
+            Donatarios.leyenda = "Donatarios leyenda";
+            Donatarios.noAutorizacion = "12545";
+
+            String XMLDonatarios = String.Empty;
+            cErrores Errores = null;
+
+            if (!Comprobante.Complementos.AgregarComplemento(Donatarios, out Errores))
+                lstErrores.DataSource = Errores;
+        }
+
+        private void AgregarComplementoiedu(Concepto cConcepto)
+        {
+            FIGeneradorFacturaElectronica.ComplementoConcepto.iedu IEDU = new iedu();
+
+            IEDU.CURP = "COTM811112HDFSMR02";
+            IEDU.rfcPago = "FID081111867";
+            IEDU.nivelEducativo = iedu.NivelesEducativos.ProfesionalTecnico;
+            IEDU.nombreAlumno = "Juan Perez";
+            IEDU.autRVOE = "I.P.N.";
+            cConcepto.ComplementoConcepto = IEDU;
+        }
+    }
+}
